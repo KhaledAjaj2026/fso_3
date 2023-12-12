30fa0264066d53dd965797fbe5a5ea3b949ccfc2
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-12-11 18:20:26 -0800

    connect backend to database

--- index.js
+++ index.js
@@ -1,50 +1,12 @@
+require('dotenv').config();
 const express = require('express');
-const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/person');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
 app.use(express.static('dist'));
-
-// Custom Morgan Token
-morgan.token('data-log', (req, res) => {
-	return JSON.stringify(req.body);
-});
-
-app.use(
-	morgan(
-		':method :data-log :url :status :res[content-length] - :response-time ms'
-	)
-);
-
-let persons = [
-	{
-		id: 1,
-		name: 'Arto Hellas',
-		number: '040-123456',
-	},
-	{
-		id: 2,
-		name: 'Ada Lovelace',
-		number: '39-44-5323523',
-	},
-	{
-		id: 3,
-		name: 'Dan Abramov',
-		number: '12-43-234345',
-	},
-	{
-		id: 4,
-		name: 'Mary Poppendieck',
-		number: '39-23-6423122',
-	},
-];
-
-const markup = `
-    <p>Phonebook has information for ${persons.length} people</p>
-    <p>${Date()}</p>
-`;
+app.use(express.json());
 
 // Main Page
 app.get('/', (req, res) => {
@@ -53,53 +15,91 @@ app.get('/', (req, res) => {
 
 // Info Page
 app.get('/info', (req, res) => {
-	res.send(markup);
+	Person.countDocuments({}).then((result) => {
+		res.send(`
+			<p>There are ${result} entries in the phonebook.</p>
+			<p>${Date()}</p>
+		`);
+	});
 });
 
 // Phonebook - All persons
 app.get('/api/persons', (req, res) => {
-	res.json(persons);
+	Person.find({}).then((people) => {
+		res.json(people);
+	});
 });
 
 // Phonebook - Single person
-app.get('/api/persons/:id', (req, res) => {
-	const id = Number(req.params.id);
-	const person = persons.find((person) => person.id === id);
-	if (person) {
-		res.json(person);
-	} else {
-		res.status(404).send(`404 - User with ID (${id}) not found`);
-	}
+app.get('/api/persons/:id', (req, res, next) => {
+	Person.findById(req.params.id)
+		.then((person) => {
+			res.json(person);
+		})
+		.catch((err) => next(err));
 });
 
 // Add Person
-app.post('/api/persons', (req, res) => {
-	const person = req.body;
-	if (!person.name || !person.number) {
-		res.status(406).json({ error: 'Name or number missing from entry' });
-	} else if (person.name) {
-		const nameToFind = person.name;
-		const existingPerson = persons.find(({ name }) => name === nameToFind);
-		if (existingPerson) {
-			res.status(409).json({ error: 'Name must be unique' });
-		} else {
-			person.id = Math.floor(Math.random() * 100 + 1);
-			res.json(person);
-		}
+app.post('/api/persons', (req, res, next) => {
+	const newPerson = req.body;
+
+	if (!newPerson) {
+		return res.status(400).json({ error: 'name or number missing' });
+	} else if (newPerson.name && newPerson.number) {
+		const person = new Person({
+			name: newPerson.name,
+			number: newPerson.number,
+		});
+
+		person
+			.save()
+			.then((newPerson) => {
+				res.json(newPerson);
+			})
+			.catch((err) => next(err));
 	}
 });
 
-// Delete person
-app.delete('/api/persons/:id', (req, res) => {
-	const id = Number(req.params.id);
-	persons = persons.filter((person) => person.id === id);
+// Update Person
+app.put('/api/persons/:id', (req, res, next) => {
+	const newPerson = req.body;
 
-	res.sendStatus(204);
+	const personData = {
+		name: newPerson.name,
+		number: newPerson.number,
+	};
+
+	Person.findByIdAndUpdate(req.params.id, personData, { new: true })
+		.then((updatedPerson) => {
+			res.json(updatedPerson);
+		})
+		.catch((err) => next(err));
+});
+
+// Delete person
+app.delete('/api/persons/:id', (req, res, next) => {
+	Person.findByIdAndDelete(req.params.id)
+		.then((result) => {
+			res.sendStatus(204);
+		})
+		.catch((err) => next(err));
 });
 
 // 404 - Not Found
 app.get('/*', (req, res) => {
 	res.sendStatus(404);
 });
+
+// Error Handler
+const errorHandler = (err, req, res, next) => {
+	console.error(err.message);
+
+	if (err.name === 'CastError') {
+		return res.status(400).send({ error: 'malformatted id' });
+	}
+
+	next(err);
+};
+app.use(errorHandler);
 
 app.listen(process.env.PORT || 3001);
